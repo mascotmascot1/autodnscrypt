8945f41eae50064f5152fe227530c2bf47dc05a3
Roman Sh. <rsheptytsky@gmail.com>
2025-06-01 23:10:36 +0400

    Set up logging to log.txt and update .gitignore

--- cmd/autodnscrypt/main.go
+++ cmd/autodnscrypt/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/mascotmascot1/autodnscrypt/internal/dnscrypt"
 )
 
 func main() {
+	const logFileName = "log.txt"
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
 	cfg := dnscrypt.LoadConfig()
 
 	ip := dnscrypt.GetIPv4FromInterface(cfg.InterfaceName)
